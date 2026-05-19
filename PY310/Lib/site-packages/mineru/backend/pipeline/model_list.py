ddcacaab55973707094ffcee8c49a87b7982ca59
ustak <1298211516@qq.com>
2026-05-19 16:09:26 +0800

    chore: 停止追踪所有 .pyc 和缓存文件

--- PY310/Lib/site-packages/mineru/backend/pipeline/model_list.py
+++
@@ -1,9 +0,0 @@
-class AtomicModel:
-    Layout = "layout"
-    MFD = "mfd"
-    MFR = "mfr"
-    OCR = "ocr"
-    WirelessTable = "wireless_table"
-    WiredTable = "wired_table"
-    TableCls = "table_cls"
-    ImgOrientationCls = "img_ori_cls"
